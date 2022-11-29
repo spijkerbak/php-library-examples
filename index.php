84e1f762ee908a6e51290b85971990c9aa01cbbc
Frans Spijkerman <frans@spijkerman.nl>
2022-11-29 20:15:09 +0100

    november updates

--- index.php
+++ index.php
@@ -8,17 +8,30 @@
         * {
             font-family: Verdana, Arial, sans-serif;
         }
-
         nav a {
             display: block;
             margin: 3px;
         }
+        footer p {
+            color: gray;
+        }
     </style>
 </head>
 
-    <body>
-        <?php for ($i = 1; $i <= 5; $i++) { ?>
-            <a target="poging-<?= $i ?>" href="poging-<?= $i ?>">Poging <?= $i ?></a>
-        <?php } ?>
-    </body>
+<body>
+    <nav>
+        <a target="poging-1" href="poging-1">Poging 1</a>
+        <a target="poging-2" href="poging-2">Poging 2</a>
+        <a target="poging-3" href="poging-3">Poging 3</a>
+        <a target="poging-4" href="poging-4">Poging 4</a>
+        <a target="poging-5" href="poging-5">Poging 5</a>
+    </nav>
+    <footer>
+        <p>
+            Get the sources from
+            <a target="github" href="https://github.com/spijkerbak/php-library-examples">github</a>.
+        </p>
+    </footer>
+</body>
+
 </html>
